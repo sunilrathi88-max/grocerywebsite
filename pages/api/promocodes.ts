7aa96468fbe9bf13ad8fab4a62f8c7b85c9b15b8
sunil rathi <sunilrathi88@gmail.com>
2025-11-11 09:32:43 +0530

    Add Next.js API route for CRUD (GET/POST) promo codes with Prisma integration.

---
+++ pages/api/promocodes.ts
@@ -0,0 +1,23 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  switch (req.method) {
+    case 'GET': {
+      // Get all promo codes
+      const promoCodes = await prisma.promoCode.findMany();
+      return res.status(200).json(promoCodes);
+    }
+    case 'POST': {
+      // Create new promo code
+      const { code, discount, discountType, active } = req.body;
+      const promoCode = await prisma.promoCode.create({
+        data: { code, discount, discountType, active }
+      });
+      return res.status(201).json(promoCode);
+    }
+    default:
+      return res.status(405).send('Method not allowed');
+  }
+}
