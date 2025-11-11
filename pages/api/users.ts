8d796a09554aa902e281f5e7193176bbefda0a1d
sunil rathi <sunilrathi88@gmail.com>
2025-11-11 09:31:16 +0530

    Add Next.js API route for CRUD (GET/POST) users with Prisma integration.

---
+++ pages/api/users.ts
@@ -0,0 +1,23 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  switch (req.method) {
+    case 'GET': {
+      // Get all users
+      const users = await prisma.user.findMany();
+      return res.status(200).json(users);
+    }
+    case 'POST': {
+      // Create new user
+      const { email, password, name, phone } = req.body;
+      const user = await prisma.user.create({
+        data: { email, password, name, phone }
+      });
+      return res.status(201).json(user);
+    }
+    default:
+      return res.status(405).send('Method not allowed');
+  }
+}
