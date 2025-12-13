1529a6c6d2c1fa9dcac14028b96de7355209227b
Sunil Rathi <sunilrathi88@gmail.com>
2025-12-13 18:08:47 +0530

    fix: resolve type mismatches in AdminDashboard and prettier syntax in App.tsx

--- App.tsx
+++ App.tsx
@@ -652,8 +652,8 @@ const App: React.FC = () => {
             addToast={addToast}
             discount={0}
             promoCode=""
-            onApplyPromoCode={() => { }}
-            onRemovePromoCode={() => { }}
+            onApplyPromoCode={() => {}}
+            onRemovePromoCode={() => {}}
             subtotal={0}
             shippingCost={0}
           />
